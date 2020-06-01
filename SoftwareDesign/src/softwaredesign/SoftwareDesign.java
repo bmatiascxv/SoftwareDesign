9bf135c71cb0cd184fa3b10d70ce8594e15630f8
bmatiascxv <matiaben@sheridancollege.ca>
2020-06-01 10:23:20 -0400

    This is a Project for the Software Design and Development Class

---
+++ SoftwareDesign/src/softwaredesign/SoftwareDesign.java
@@ -0,0 +1,16 @@
+package softwaredesign;
+
+/**
+ *
+ * @author benma
+ */
+public class SoftwareDesign {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
